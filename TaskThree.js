7453fe742ee2a07d6caec00ff198abe9a7bcc1c2
Pankyes <praisemandik8@gmail.com>
2026-05-07 01:31:53 +0100

    more meatpie

---
+++ TaskThree.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+a = +prompt("Enter number")
+b = +prompt("Enter second number")
+sum = a +b
+console.log(sum)
---
+++ taskNine.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")();
+let color = prompt("WHat's your favorite color?")
+console.log(color)
