7db5706996e3c88a0e0f9988f6c76e8ca0e987f8
Pankyes <praisemandik8@gmail.com>
2026-05-08 01:02:23 +0100

    done

---
+++ taskTeight.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")();
+
+let number = prompt("Enter Number ")
+console.log(number/3)
---
+++ taskThirty.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let num1 = +prompt("Enter Number ")
+let num2 = +prompt("Enter Number ")
+console.log("sum of numbers is", num1+num2)
+console.log("product of numbers is", num1*num2)
